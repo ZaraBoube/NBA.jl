64399d2c743e39b2b3bc4f3d484a40a765a6e3c8
ZaraBoube <zaraboube18@gmail.com>
2022-11-08 09:35:55 +0100

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -1,21 +1,30 @@
 
 #les fonctions utiles
-@doc
+
 """
-fonction test
+fonction test qui renvoie hey
 """
 function tester()
     println("hey")
 end
-@doc
+
 """
 #Arguments
 - `fichier :: fichier `: le chemin vers le fichier à utiliser
+@renvoie la base de donnée
 """
 function ajouter(fichier)
      data=CSV.read(fichier) 
      return data
 end
+
+"""
+#Arguments
+-`data :: datasets ` : base de donnée utilisée
+-`numcolonne :: Integer `: numero de la colonne
+-`nouveaunom :: String `: nom de la colonne
+@renvoie le nouveau nom de la colonne
+"""
 function renommer(data, numcolonne ,nouveaunom)
     rename(data)[numcolonne]=nouveaunom
     return name(data[numcolonne])
